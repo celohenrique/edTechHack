a258606805118d935614e843efa6223b6310789f
Marcelo Henrique <celohenriquech7@gmail.com>
2023-05-25 16:15:49 -0300

    Initial Commit

---
+++ edTechHack/edTechHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  edTechHackApp.swift
+//  edTechHack
+//
+//  Created by Marcelo Araujo on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct edTechHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
